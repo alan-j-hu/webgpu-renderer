2548b896a0e0df260404712ba8dff50e3b6e875c
Alan Hu <ahulambda@gmail.com>
2025-04-02 18:49:16 -0400

    Add MeshBuilder helper class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "noworry/application.h"
 #include "noworry/camera.h"
 #include "noworry/mesh.h"
+#include "noworry/meshbuilder.h"
 #include "noworry/model.h"
 #include "noworry/renderer.h"
 #include "noworry/resourcetable.h"
@@ -8,38 +9,6 @@
 #include <array>
 #include <glm/ext/scalar_constants.hpp>
 #include <webgpu/webgpu.h>
-
-Vertex make_vertex(
-    float x, float y, float z, float u, float v)
-{
-    Vertex vertex = { 0 };
-    vertex.x = x;
-    vertex.y = y;
-    vertex.z = z;
-    vertex.u = u;
-    vertex.v = v;
-    return vertex;
-}
-
-void make_quad(
-    Vertex* vs,
-    std::uint16_t* is,
-    int v_off,
-    int i_off,
-    Vertex a, Vertex b, Vertex c, Vertex d)
-{
-    vs[v_off] = a;
-    vs[v_off + 1] = b;
-    vs[v_off + 2] = c;
-    vs[v_off + 3] = d;
-
-    is[i_off] = v_off;
-    is[i_off + 1] = v_off + 1;
-    is[i_off + 2] = v_off + 2;
-    is[i_off + 3] = v_off;
-    is[i_off + 4] = v_off + 2;
-    is[i_off + 5] = v_off + 3;
-}
 
 class Main : public Application
 {
@@ -51,48 +20,28 @@ public:
     {
         m_renderer.set_clear_color({0.5, 0.5, 0.5, 1});
 
-        std::array<Vertex, 16> vertices;
-        std::array<std::uint16_t, 24> indices;
-        // front
-        make_quad(
-            vertices.data(),
-            indices.data(),
-            0,
-            0,
-            make_vertex(-20, 20, -20, 0, 0),
-            make_vertex(-20, -20, -20, 1, 0),
-            make_vertex(20, -20, -20, 1, 1),
-            make_vertex(20, 20, -20, 0, 1));
-        // right
-        make_quad(
-            vertices.data(),
-            indices.data(),
-            4,
-            6,
-            make_vertex(20, 20, -20, 0, 0),
-            make_vertex(20, -20, -20, 1, 0),
-            make_vertex(20, -20, 20, 1, 1),
-            make_vertex(20, 20, 20, 0, 1));
-        // back
-        make_quad(
-            vertices.data(),
-            indices.data(),
-            8,
-            12,
-            make_vertex(20, 20, 20, 0, 0),
-            make_vertex(20, -20, 20, 1, 0),
-            make_vertex(-20, -20, 20, 1, 1),
-            make_vertex(-20, 20, 20, 0, 1));
-        // left
-        make_quad(
-            vertices.data(),
-            indices.data(),
-            12,
-            18,
-            make_vertex(-20, 20, 20, 0, 0),
-            make_vertex(-20, -20, 20, 1, 0),
-            make_vertex(-20, -20, -20, 1, 1),
-            make_vertex(-20, 20, -20, 0, 1));
+        auto& mesh = MeshBuilder()
+            .quad( // front
+                XYZUV(-20, 20, -20, 0, 0),
+                XYZUV(-20, -20, -20, 0, 1),
+                XYZUV(20, -20, -20, 1, 1),
+                XYZUV(20, 20, -20, 1, 0))
+            .quad( // right
+                XYZUV(20, 20, -20, 0, 0),
+                XYZUV(20, -20, -20, 0, 1),
+                XYZUV(20, -20, 20, 1, 1),
+                XYZUV(20, 20, 20, 1, 0))
+            .quad( // back
+                XYZUV(20, 20, 20, 0, 0),
+                XYZUV(20, -20, 20, 0, 1),
+                XYZUV(-20, -20, 20, 1, 1),
+                XYZUV(-20, 20, 20, 1, 0))
+            .quad( // left
+                XYZUV(-20, 20, 20, 0, 0),
+                XYZUV(-20, -20, 20, 0, 1),
+                XYZUV(-20, -20, -20, 1, 1),
+                XYZUV(-20, 20, -20, 1, 0))
+            .build(m_resources);
 
         auto& texture = m_resources.add_texture("../assets/cat.png");
         auto& material = m_resources.add_texture_material(texture);
@@ -100,9 +49,7 @@ public:
         auto& material2 = m_resources.add_flat_material(1, 0, 0);
 
         m_yaw = glm::pi<float>() * 0.25f;
-        auto& mesh = m_resources.add_mesh(
-            vertices.data(), 16, indices.data(), 8);
-        m_model = &m_resources.add_model(mesh, material2);
+        m_model = &m_resources.add_model(mesh, material);
         m_model->set_translation(glm::vec3(0.0f, 0.0f, 50.0f));
 
         auto& camera = m_renderer.camera();
---
+++ src/meshbuilder.cpp
@@ -0,0 +1,57 @@
+#include "noworry/meshbuilder.h"
+
+MeshBuilder& MeshBuilder::triangle(XYZ a, XYZ b, XYZ c)
+{
+    return triangle(
+        XYZUV(a.x, a.y, a.z, 0, 0),
+        XYZUV(b.x, b.y, b.z, 0, 0),
+        XYZUV(c.x, c.y, c.z, 0, 0));
+}
+
+MeshBuilder& MeshBuilder::triangle(XYZUV a, XYZUV b, XYZUV c)
+{
+    int idx = m_vertices.size();
+    m_vertices.push_back(a.to_vertex());
+    m_vertices.push_back(b.to_vertex());
+    m_vertices.push_back(c.to_vertex());
+
+    m_indices.push_back(idx);
+    m_indices.push_back(idx + 1);
+    m_indices.push_back(idx + 2);
+
+    return *this;
+}
+
+MeshBuilder& MeshBuilder::quad(XYZ a, XYZ b, XYZ c, XYZ d)
+{
+    return quad(
+        XYZUV(a.x, a.y, a.z, 0, 0),
+        XYZUV(b.x, b.y, b.z, 0, 0),
+        XYZUV(c.x, c.y, c.z, 0, 0),
+        XYZUV(d.x, d.y, d.z, 0, 0));
+}
+
+MeshBuilder& MeshBuilder::quad(XYZUV a, XYZUV b, XYZUV c, XYZUV d)
+{
+    int idx = m_vertices.size();
+    m_vertices.push_back(a.to_vertex());
+    m_vertices.push_back(b.to_vertex());
+    m_vertices.push_back(c.to_vertex());
+    m_vertices.push_back(d.to_vertex());
+
+    m_indices.push_back(idx);
+    m_indices.push_back(idx + 1);
+    m_indices.push_back(idx + 2);
+    m_indices.push_back(idx);
+    m_indices.push_back(idx + 2);
+    m_indices.push_back(idx + 3);
+
+    return *this;
+}
+
+Mesh& MeshBuilder::build(ResourceTable& table)
+{
+    return table.add_mesh(
+        m_vertices.data(), m_vertices.size(),
+        m_indices.data(), m_indices.size() / 3);
+}
